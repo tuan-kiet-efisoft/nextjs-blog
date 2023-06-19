ac8061f707921b7d89a5ea9eb02188cac3cb6850
tuan-kiet-efisoft <kiet.dao@efisoft.com.vn>
2023-06-19 10:26:59 +0700

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData?.title}</title>
+      </Head>
+      {postData?.id}
+      <br />
+      {postData?.title}
+      <br />
+      {postData?.body}
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+    const paths = await getAllPostIds()
+    return {
+        paths: [],
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
