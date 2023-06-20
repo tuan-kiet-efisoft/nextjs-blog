c11fe194323081d54b512d78675a7162b5354198
tuan-kiet-efisoft <kiet.dao@efisoft.com.vn>
2023-06-20 12:01:32 +0700

    edit ssr to ssg

--- pages/index.js
+++ pages/index.js
@@ -36,10 +36,6 @@ export default function Home({ allPostsData }) {
 }
 
 export async function getServerSideProps({ req, res }) {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
   const allPostsData = await getSortedPostsData()
   return {
     props: {
